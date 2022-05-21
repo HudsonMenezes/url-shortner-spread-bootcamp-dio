fa217e53ccb9789fcdf89117ed20aba3932596f7
Hudson Menezes <hudson.bm23@gmail.com>
2022-05-21 19:31:34 -0300

    finished project

---
+++ dist/config/Constants.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.config = void 0;
+exports.config = {
+    API_URL: "localhost:5000",
+    MONGO_CONNECTION: 'mongodb+srv://admin:admin@encurtador-url-dio.ounhj.mongodb.net/?retryWrites=true&w=majority'
+};
+//# sourceMappingURL=Constants.js.map
---
+++ src/config/Constants.ts
@@ -0,0 +1,5 @@
+export const config = {
+  API_URL: "localhost:5000",
+  MONGO_CONNECTION:
+  'mongodb+srv://admin:admin@encurtador-url-dio.ounhj.mongodb.net/?retryWrites=true&w=majority'
+}
